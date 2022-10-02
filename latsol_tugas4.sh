91390d99c7cb080dd09c22ee01d2f09e80c5a561
SintiyaRistiyani <113277452+SintiyaRistiyani@users.noreply.github.com>
2022-10-02 22:45:43 +0700

    Tugas4

---
+++ latsol_tugas4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "input : " input
+printf "\n"
+
+if ((input % 2 == 0));
+then
+       let input=input-1;
+fi
+for ((angka=input; angka>0; angka=angka-2))
+do
+         printf "$angka\n"
+done
